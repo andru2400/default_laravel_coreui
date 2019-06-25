6331f21abb5b33ed498727c58df0e201f8a54189
Andres David P <andru2400@hotmail.com>
2019-06-25 17:11:39 -0500

    first commit

---
+++ resources/views/back/layout/home/home.blade.php
@@ -0,0 +1,8 @@
+@extends('back.template.main')
+@section('additional_script') 
+    {{-- <script src="{{ asset('js/back/account/campaign.js') }}"></script> --}}
+    {{-- <link href="{{ asset('css/campaign.css') }}" rel="stylesheet"> --}}
+@endsection
+@section('content') 
+    <h1>Bienvenido al Home del back!!!</h1>
+@endsection 
